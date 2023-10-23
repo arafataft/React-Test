874b1bf914f0f5f852fb864d8f11f6c1b3269360
arafataft <arafatcd@gmail.com>
2023-10-24 02:42:09 +0600

    Final commit

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,25 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
-
     const [show, setShow] = useState('all');
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [tableData, setTableData] = useState([]);
 
-    const handleClick = (val) =>{
+    const handleClick = (val) => {
         setShow(val);
     }
 
-    return (
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTableData([...tableData, { name, status }]);
+        setName('');
+        setStatus('');
+    }
 
+    const sortedData = tableData.sort((a, b) => {
+        if (a.status === 'Active' && b.status !== 'Active') return -1;
+        if (a.status !== 'Active' && b.status === 'Active') return 1;
+        if (a.status === 'Complete' && b.status !== 'Complete') return -1;
+        if (a.status !== 'Complete' && b.status === 'Complete') return 1;
+        return 0;
+    });
+
+    const filteredData = show === 'all'
+        ? sortedData
+        : sortedData.filter(item => (show === 'active' && item.status === 'Active') || (show === 'completed' && item.status === 'Complete'));
+
+    return (
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                <div className="col-6">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={handleSubmit}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Status"
+                                value={status}
+                                onChange={(e) => setStatus(e.target.value)}
+                            />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,25 +61,30 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>Complete</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
-                    <table className="table table-striped ">
+                    <table className="table table-striped">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {filteredData.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>{item.status}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,194 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Problem2 = () => {
+    const [showModalA, setShowModalA] = useState(false);
+    const [showModalB, setShowModalB] = useState(false);
+    const [modalContent, setModalContent] = useState([]);
+    const [currentModal, setCurrentModal] = useState(null);
+    const [showEvenIds, setShowEvenIds] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get('https://contact.mediusware.com/api/contacts/?format=json')
+            .then((response) => {
+                const responseData = response.data;
+                if(responseData && responseData.results) {
+                    setModalContent(responseData.results);
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    const openModal = (modal) => {
+        if (modal === 'A') {
+            setShowModalA(true);
+            setShowModalB(false);
+            setCurrentModal('A');
+        } else if (modal === 'B') {
+            setShowModalB(true);
+            setShowModalA(false);
+            setCurrentModal('B');
+        }
+    };
+
+    const closeModal = () => {
+        setShowModalA(false);
+        setShowModalB(false);
+        setCurrentModal(null);
+    };
+
+    const modalContentB = modalContent.filter((item) => item.country.name === 'United States');
+
+    const filteredModalContent = showEvenIds ? modalContent.filter((item) => item.id % 2 === 0) : modalContent;
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+                <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button
+                        className="btn btn-lg"
+                        style={{ backgroundColor: '#46139f', color: 'white' }}
+                        type="button"
+                        onClick={() => openModal('A')}
+                    >
+                        All Contacts
+                    </button>
+                    <button
+                        className="btn btn-lg"
+                        style={{ backgroundColor: '#ff7f50', color: 'white' }}
+                        type="button"
+                        onClick={() => openModal('B')}
+                    >
+                        US Contacts
+                    </button>
                 </div>
-                
+            </div>
+
+            {/* Modal A */}
+            <div
+                className={`modal fade ${showModalA ? 'show' : ''}`}
+                id="modalA"
+                tabIndex="-1"
+                role="dialog"
+                style={{ display: showModalA ? 'block' : 'none' }}
+            >
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <h2 className='text-center'>ALL Contacts</h2>
+                        
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Phone</th>
+                                    <th>Country</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredModalContent.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.id}</td>
+                                        <td>{item.phone}</td>
+                                        <td>{item.country.name}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    checked={showEvenIds}
+                                    onChange={() => setShowEvenIds(!showEvenIds)}
+                                />{' '}
+                                Show Even IDs
+                            </label>
+                        </div>
+                        <div className="d-flex">
+                            <button
+                                className="btn m-1"
+                                style={{ backgroundColor: '#46139f', color: 'white' }}
+                                onClick={() => openModal('A')}
+                            >
+                                All Contacts
+                            </button>
+                            <button
+                                className="btn m-1"
+                                style={{ backgroundColor: '#ff7f50', color: 'white' }}
+                                onClick={() => openModal('B')}
+                            >
+                                US Contacts
+                            </button>
+                            <button
+                                className="btn m-1"
+                                style={{ backgroundColor: 'white', color: '#46139f', border: '1px solid #46139f' }}
+                                onClick={closeModal}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Modal B */}
+            <div
+                className={`modal fade ${showModalB ? 'show' : ''}`}
+                id="modalB"
+                tabIndex="-1"
+                role="dialog"
+                style={{ display: showModalB ? 'block' : 'none' }}
+            >
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <h2 className='text-center'>US Contacts</h2>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Phone</th>
+                                    <th>Country</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {modalContentB.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.id}</td>
+                                        <td>{item.phone}</td>
+                                        <td>{item.country.name}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div className="d-flex">
+                            <button
+                                className="btn m-1"
+                                style={{ backgroundColor: '#46139f', color: 'white' }}
+                                onClick={() => openModal('A')}
+                            >
+                                All Contacts
+                            </button>
+                            <button
+                                className="btn m-1"
+                                style={{ backgroundColor: '#ff7f50', color: 'white' }}
+                                onClick={() => openModal('B')}
+                            >
+                                US Contacts
+                            </button>
+                            <button
+                                className="btn m-1"
+                                style={{ backgroundColor: 'white', color: '#46139f', border: '1px solid #46139f' }}
+                                onClick={closeModal}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
